0e208c03ef2fff5945235cd9ec22619538454321
George <ubercharge.ready@gmail.com>
2016-08-19 11:09:58 +0300

    work on theme improvements

--- app/src/main/java/com/gDyejeekis/aliencompanion/Fragments/DialogFragments/BaseThemesDialogFragment.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/Fragments/DialogFragments/BaseThemesDialogFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -29,35 +30,35 @@ public class BaseThemesDialogFragment extends ScalableDialogFragment implements 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_base_themes, container, false);
 
-        RadioButton light = (RadioButton) view.findViewById(R.id.radioButtonLight);
-        RadioButton materialBlue = (RadioButton) view.findViewById(R.id.radioButtonMaterialBlue);
-        RadioButton materialGrey = (RadioButton) view.findViewById(R.id.radioButtonMaterialGrey);
-        RadioButton dark = (RadioButton) view.findViewById(R.id.radioButtonDark);
-        RadioButton darkLowContrast = (RadioButton) view.findViewById(R.id.radioButtonDarkLowContrast);
 
         switch (MyApplication.currentBaseTheme) {
             case MyApplication.LIGHT_THEME:
+                RadioButton light = (RadioButton) view.findViewById(R.id.radioButtonLight);
                 light.setChecked(true);
                 break;
             case MyApplication.MATERIAL_BLUE_THEME:
+                RadioButton materialBlue = (RadioButton) view.findViewById(R.id.radioButtonMaterialBlue);
                 materialBlue.setChecked(true);
                 break;
             case MyApplication.MATERIAL_GREY_THEME:
+                RadioButton materialGrey = (RadioButton) view.findViewById(R.id.radioButtonMaterialGrey);
                 materialGrey.setChecked(true);
                 break;
             case MyApplication.DARK_THEME:
+                RadioButton dark = (RadioButton) view.findViewById(R.id.radioButtonDark);
                 dark.setChecked(true);
                 break;
             case MyApplication.DARK_THEME_LOW_CONTRAST:
+                RadioButton darkLowContrast = (RadioButton) view.findViewById(R.id.radioButtonDarkLowContrast);
                 darkLowContrast.setChecked(true);
                 break;
         }
 
-        light.setOnClickListener(this);
-        materialBlue.setOnClickListener(this);
-        materialGrey.setOnClickListener(this);
-        dark.setOnClickListener(this);
-        darkLowContrast.setOnClickListener(this);
+        (view.findViewById(R.id.light)).setOnClickListener(this);
+        (view.findViewById(R.id.material_blue)).setOnClickListener(this);
+        (view.findViewById(R.id.material_grey)).setOnClickListener(this);
+        (view.findViewById(R.id.dark)).setOnClickListener(this);
+        (view.findViewById(R.id.dark_low_contrast)).setOnClickListener(this);
 
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
         getDialog().setCanceledOnTouchOutside(true);
@@ -69,23 +70,23 @@ public class BaseThemesDialogFragment extends ScalableDialogFragment implements 
         int theme = MyApplication.currentBaseTheme;
         boolean night = MyApplication.nightThemeEnabled;
         switch (v.getId()) {
-            case R.id.radioButtonLight:
+            case R.id.light:
                 theme = MyApplication.LIGHT_THEME;
                 night = false;
                 break;
-            case R.id.radioButtonMaterialBlue:
+            case R.id.material_blue:
                 theme = MyApplication.MATERIAL_BLUE_THEME;
                 night = true;
                 break;
-            case R.id.radioButtonMaterialGrey:
+            case R.id.material_grey:
                 theme = MyApplication.MATERIAL_GREY_THEME;
                 night = true;
                 break;
-            case R.id.radioButtonDark:
+            case R.id.dark:
                 theme = MyApplication.DARK_THEME;
                 night = true;
                 break;
-            case R.id.radioButtonDarkLowContrast:
+            case R.id.dark_low_contrast:
                 theme = MyApplication.DARK_THEME_LOW_CONTRAST;
                 night = true;
                 break;
