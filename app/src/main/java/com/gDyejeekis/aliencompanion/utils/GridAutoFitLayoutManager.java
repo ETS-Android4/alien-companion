467e6df884bf796fe6117f9e8d6de4aebdc708a1
George <ubercharge.ready@gmail.com>
2017-02-08 17:07:16 +0200

    fix grid autofit layout manager

--- app/src/main/java/com/gDyejeekis/aliencompanion/utils/GridAutoFitLayoutManager.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/utils/GridAutoFitLayoutManager.java
@@ -3,31 +3,33 @@ package com.gDyejeekis.aliencompanion.utils;
 import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.TypedValue;
 
-public class GridAutoFitLayoutManager extends GridLayoutManager
-{
-    private int mColumnWidth;
-    private boolean mColumnWidthChanged = true;
+public class GridAutoFitLayoutManager extends GridLayoutManager {
 
-    public GridAutoFitLayoutManager(Context context, int columnWidth)
-    {
+    public static final String TAG = "GridAutoFitLayout";
+
+    private int mColumnWidth;
+    private float mDensity;
+    //private boolean mColumnWidthChanged = true;
+
+    public GridAutoFitLayoutManager(Context context, int columnWidth) {
         /* Initially set spanCount to 1, will be changed automatically later. */
         super(context, 1);
         setColumnWidth(checkedColumnWidth(context, columnWidth));
+        setDensity(context);
     }
 
-    public GridAutoFitLayoutManager(Context context, int columnWidth, int orientation, boolean reverseLayout)
-    {
+    public GridAutoFitLayoutManager(Context context, int columnWidth, int orientation, boolean reverseLayout) {
         /* Initially set spanCount to 1, will be changed automatically later. */
         super(context, 1, orientation, reverseLayout);
         setColumnWidth(checkedColumnWidth(context, columnWidth));
+        setDensity(context);
     }
 
-    private int checkedColumnWidth(Context context, int columnWidth)
-    {
-        if (columnWidth <= 0)
-        {
+    private int checkedColumnWidth(Context context, int columnWidth) {
+        if (columnWidth <= 0) {
             /* Set default columnWidth value (48dp here). It is better to move this constant
             to static constant on top, but we need context to convert it to dp, so can't really
             do so. */
@@ -37,34 +39,42 @@ public class GridAutoFitLayoutManager extends GridLayoutManager
         return columnWidth;
     }
 
-    public void setColumnWidth(int newColumnWidth)
-    {
+    public void setColumnWidth(int newColumnWidth) {
         if (newColumnWidth > 0 && newColumnWidth != mColumnWidth)
         {
             mColumnWidth = newColumnWidth;
-            mColumnWidthChanged = true;
+            //mColumnWidthChanged = true;
         }
     }
 
+    private void setDensity(Context context) {
+        mDensity = context.getResources().getDisplayMetrics().density;
+    }
+
     @Override
-    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state)
-    {
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        //Log.d(TAG, "-----------------------------------------------");
+        //Log.d(TAG, "onLayoutChildren");
         int width = getWidth();
         int height = getHeight();
-        if (mColumnWidthChanged && mColumnWidth > 0 && width > 0 && height > 0)
-        {
+        //Log.d(TAG, width + " width");
+        //Log.d(TAG, height + " height");
+        if (/*mColumnWidthChanged && */mColumnWidth > 0 && width > 0 && height > 0) {
             int totalSpace;
-            if (getOrientation() == VERTICAL)
-            {
+            if (getOrientation() == VERTICAL) {
                 totalSpace = width - getPaddingRight() - getPaddingLeft();
             }
-            else
-            {
+            else {
                 totalSpace = height - getPaddingTop() - getPaddingBottom();
             }
-            int spanCount = Math.max(1, totalSpace / mColumnWidth);
+            float dpWidth = totalSpace / mDensity;
+            int spanCount = Math.max(1, (int) dpWidth / mColumnWidth);
+            if(spanCount==1) {
+                spanCount++;
+            }
+            //Log.d(TAG, spanCount + " spans");
             setSpanCount(spanCount);
-            mColumnWidthChanged = false;
+            //mColumnWidthChanged = false;
         }
         super.onLayoutChildren(recycler, state);
     }
