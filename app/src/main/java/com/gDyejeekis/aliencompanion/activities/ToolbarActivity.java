9b5134e4ed0ee1604e77b44e279af83249a3c8db
George <ubercharge.ready@gmail.com>
2017-03-22 19:30:37 +0200

    fix update status bar on color change

--- app/src/main/java/com/gDyejeekis/aliencompanion/activities/ToolbarActivity.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/activities/ToolbarActivity.java
@@ -1,18 +1,9 @@
 package com.gDyejeekis.aliencompanion.activities;
 
 import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
-import android.view.animation.Interpolator;
-import android.view.animation.TranslateAnimation;
 
 import com.gDyejeekis.aliencompanion.MyApplication;
 import com.gDyejeekis.aliencompanion.R;
@@ -42,12 +33,11 @@ public abstract class ToolbarActivity extends BackNavActivity {
         }
     }
 
-    public void  updateToolbarColor() {
-        updateToolbarColor(MyApplication.colorPrimary);
-    }
-
-    public void updateToolbarColor(int color) {
-        toolbar.setBackgroundColor(color);
+    protected void updateToolbarColors() {
+        toolbar.setBackgroundColor(MyApplication.currentColor);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(MyApplication.colorPrimaryDark);
+        }
     }
 
     protected void initToolbar() {
@@ -56,17 +46,10 @@ public abstract class ToolbarActivity extends BackNavActivity {
         if(MyApplication.nightThemeEnabled) {
             toolbar.setPopupTheme(R.style.OverflowStyleDark);
         }
-        toolbar.setBackgroundColor(MyApplication.currentColor);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(MyApplication.colorPrimaryDark);
-        }
+        updateToolbarColors();
         toolbar.setNavigationIcon(MyApplication.homeAsUpIndicator);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    //@Override
-    //protected void onCreate(@Nullable Bundle savedInstanceState) {
-    //    super.onCreate(savedInstanceState);
-    //}
 }
