bfaaa527ab3c985ddd12f5cf5d2e1f5863bfcd0e
George <ubercharge.ready@gmail.com>
2017-05-14 19:05:34 +0300

    tweak scalable dialog width

--- app/src/main/java/com/gDyejeekis/aliencompanion/fragments/dialog_fragments/ScalableDialogFragment.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/fragments/dialog_fragments/ScalableDialogFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.DialogFragment;
 import android.view.Window;
 import android.widget.LinearLayout;
 
+import com.gDyejeekis.aliencompanion.MyApplication;
+
 /**
  * Created by sound on 10/6/2015.
  */
@@ -23,8 +25,20 @@ public abstract class ScalableDialogFragment extends DialogFragment {
     }
 
     private void setDialogWidth() {
+        boolean isLandscape = getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        float widthFactor;
+        if(MyApplication.isLargeScreen) {
+            widthFactor = isLandscape ? 0.6f : 0.8f;
+        }
+        else if(MyApplication.isVeryLargeScreen) {
+            widthFactor = isLandscape ? 0.5f : 0.7f;
+        }
+        else {
+            widthFactor = isLandscape ? 0.79f : 0.99f;
+        }
+        int width = Math.round(getResources().getDisplayMetrics().widthPixels * widthFactor);
         Window window = getDialog().getWindow();
-        int width = 6 * getResources().getDisplayMetrics().widthPixels / 7;
         window.setLayout(width, LinearLayout.LayoutParams.WRAP_CONTENT);
     }
+
 }
