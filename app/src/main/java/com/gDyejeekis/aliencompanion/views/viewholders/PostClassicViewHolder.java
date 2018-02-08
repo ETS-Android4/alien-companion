c2955b4a2bd471041c16249a1a6cc0ef71b98cb2
George <ubercharge.ready@gmail.com>
2018-02-08 16:06:26 +0200

    tweak icon opacity

--- app/src/main/java/com/gDyejeekis/aliencompanion/views/viewholders/PostClassicViewHolder.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/views/viewholders/PostClassicViewHolder.java
@@ -88,7 +88,7 @@ public class PostClassicViewHolder extends PostViewHolder  {
         switch (MyApplication.currentBaseTheme) {
             case MyApplication.LIGHT_THEME:
                 defaultIconOpacity = 0.54f;
-                defaultIconOpacityDisabled = 0.38f;
+                defaultIconOpacityDisabled = 0.27f;
                 break;
             case MyApplication.DARK_THEME_LOW_CONTRAST:
                 defaultIconOpacity = 0.6f;
