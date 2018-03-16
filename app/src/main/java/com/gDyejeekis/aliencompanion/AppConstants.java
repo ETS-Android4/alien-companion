c6f7da49fce6f9f55d5ac7ec924486c046d0bd3f
George <ubercharge.ready@gmail.com>
2018-03-16 22:49:52 +0200

    increment version

--- app/src/main/java/com/gDyejeekis/aliencompanion/AppConstants.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/AppConstants.java
@@ -6,9 +6,9 @@ package com.gDyejeekis.aliencompanion;
 
 public class AppConstants {
 
-    public static final String currentVersion = "1.0.4";
+    public static final String currentVersion = "1.0.5";
 
-    public static final int currentVersionCode = 1004;
+    public static final int currentVersionCode = 1005;
 
     public static final int showWelcomeMsgVersionCode = -1;
 
