4b9e186a256fadaad212aa0a53300d98d804a315
George <ubercharge.ready@gmail.com>
2018-04-01 19:56:57 +0300

    fix illegal state exception

--- app/src/main/java/com/gDyejeekis/aliencompanion/fragments/dialog_fragments/MoveAppDataDialogFragment.java
+++ app/src/main/java/com/gDyejeekis/aliencompanion/fragments/dialog_fragments/MoveAppDataDialogFragment.java
@@ -105,7 +105,7 @@ public class MoveAppDataDialogFragment extends DialogFragment {
 
             @Override
             protected void onPostExecute(Void aVoid) {
-                dismiss();
+                dismissAllowingStateLoss();
             }
         }.execute();
 
